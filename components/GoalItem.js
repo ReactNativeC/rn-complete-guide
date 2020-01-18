c31b5b5b0cbb8c9842377a1bbaf4fba0c5318b0a
Noor <noormahammad@users.noreply.github.com>
2020-01-18 11:04:46 -0500

    TouchableOpacity LongPressed Event

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight} from 'react-native';
 
 const GoalItem = (props) => {
+  const LongPressed = () => { alert('you long pressed')}
+  const Pressed = () => { alert("pressed") }
+  
   return (
-    <View style={styles.listitem}>
-      <Text>{props.children}</Text>
-    </View>             
+    <TouchableOpacity onLongPress={LongPressed} onPress={Pressed}>
+      <View style={styles.listitem}>
+        <Text>{props.children}</Text>
+      </View>           
+    </TouchableOpacity>  
   );
 }
 
